35584ef22d5e308ad1596d9a7cc0432fd389c473
Luis MOlina <luismol@umich.edu>
2015-03-19 05:47:02 +0000

    More to the server

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@ import os, uuid
 from proxylib import Proxylib
  
 __UPLOADS__ = "uploads/"
+#proxylib = Proxylib()
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -21,10 +22,19 @@ class UploadHandler(tornado.web.RequestHandler):
         print "fileinfo is", fileinfo
         fname = fileinfo['filename']
         extn = os.path.splitext(fname)[1]
-        cname = str(uuid.uuid4()) + extn
-        fh = open(__UPLOADS__ + user_id + "/" + cname, 'w')
+        fname += extn
+        directory = __UPLOADS__ + user_id + "/"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        fh = open(directory + fname, 'w')
         fh.write(fileinfo['body'])
-        self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+
+        # Reencrypt the file I just received
+        proxylib = Proxylib()
+        proxylib.reencrypt("LuisKey_sFriendKey_p", directory + fname, "reencryption_file")
+
+        # TODO send to all the friends or store somewhere to be used
+        self.finish(fname + " is uploaded!! Check %s folder" %__UPLOADS__)
 
 class DownloadHandler(tornado.web.RequestHandler):
     def get(self, user_id):
