2f3acd3fdeb57da055590ac54aff1da49bcefd64
Luis MOlina <luismol@umich.edu>
2015-03-19 05:20:00 +0000

    Code for the client and server

--- server/server.py
+++ server/server.py
@@ -13,10 +13,24 @@ class MainHandler(tornado.web.RequestHandler):
         #self.render("openID_login.html")
 
 class UploadHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Upload a file with POST")
+    def get(self, user_id):
+        self.write("Upload a file with POST for user: %s" %user_id)
 
-    def post(self):
+    def post(self, user_id):
+        fileinfo = self.request.files['file'][0]
+        print "fileinfo is", fileinfo
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        cname = str(uuid.uuid4()) + extn
+        fh = open(__UPLOADS__ + user_id + "/" + cname, 'w')
+        fh.write(fileinfo['body'])
+        self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+
+class DownloadHandler(tornado.web.RequestHandler):
+    def get(self, user_id):
+        self.write("Upload a file with POST for user: %s" %user_id)
+
+    def post(self, user_id):
         fileinfo = self.request.files['file'][0]
         print "fileinfo is", fileinfo
         fname = fileinfo['filename']
@@ -28,7 +42,8 @@ class UploadHandler(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/upload", UploadHandler),
+    (r"/upload/([^/]+)", UploadHandler),
+    (r"/download/([^/]+)", DownloadHandler),
 ])
 
 if __name__ == "__main__":
