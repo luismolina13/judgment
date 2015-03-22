e0ffe62d45370f6cc8158e3329631402ecb24e12
ldion <ldion78@gmail.com>
2015-03-22 14:43:08 -0400

    client.py for downloads

--- server/client.py
+++ server/client.py
@@ -1,8 +1,20 @@
 import requests
+from tornado import httpclient
+http_client = httpclient.HTTPClient()
+url='http://ec2-54-92-44-89.ap-northeast-1.compute.amazonaws.com/download/1'
+count = 1
+try:
+    response = http_client.fetch(url)
+    with open("file" + str(count) + ".zip", 'w') as f:
+	f.write(response.body)
+	count += 1
+except httpclient.HTTPError as e:
+    print("Error:", e)
+http_client.close()
 
-url1='http://ec2-54-65-123-251.ap-northeast-1.compute.amazonaws.com/upload/120'
+#url1='http://ec2-54-92-44-89.ap-northeast-1.compute.amazonaws.com/upload/0'
 
-filehandle = open('./LuisKey_sFriendKey_p')
-r = requests.post(url1, data={},files = {'file':filehandle})
-print r.status_code
-print r.text
+#filehandle = open('./file1')
+#r = requests.post(url1, data={},files = {'file':filehandle})
+#print r.status_code
+#print r.text
