6dd9426034a76d06b1c2fd0447c55ab11c2383c5
Luis MOlina <luismol@umich.edu>
2015-04-02 13:39:07 +0000

    Removed code from proxylib

--- server/proxylib.py
+++ server/proxylib.py
@@ -23,7 +23,7 @@ class Proxylib(object):
     def decrypt(self, secret_key, enc_file_name, dec_file_name):
         lib.Proxylib_decrypt(self.obj, secret_key, enc_file_name, dec_file_name)
 
-f = Proxylib()
+# f = Proxylib()
 # f.bar("Luis") #and you will see "Hello" on the screen
 # f.generate_key("LuisKey")
 # f.generate_key("FriendKey")
@@ -31,21 +31,21 @@ f = Proxylib()
 # f.encrypt("LuisKey_p", "http://www.cplusplus.com/reference/cstring/strcpy/", "encryption")
 # f.reencrypt("LuisKey_sFriendKey_p", "encryption", "reencryption")
 # f.decrypt("FriendKey_s", "reencryption", "decryption")
-with open('100_node_links.txt', 'r') as fileInput:
-    for line in fileInput:
-        friends = line.split()
-        friend1p = "files/public_keys/" + str(friends[0]) + "_p"
-        friend1s = "files/public_keys/" + str(friends[0]) + "_s"
-        friend2p = "files/public_keys/" + str(friends[1]) + "_p"
-        friend2s = "files/public_keys/" + str(friends[1]) + "_s"
-        print "friend1p:", friend1p
-        print "friend1s:", friend1s
-        print "friend2p:", friend2p
-        print "friend2s:", friend2s
-        friend1_sfriend2_p = "files/"+str(friends[0])+"/del_keys/"+str(friends[0])+"_s"+str(friends[1])+"_p"
-        friend2_sfriend1_p = "files/"+str(friends[1])+"/del_keys/"+str(friends[1])+"_s"+str(friends[0])+"_p"
-        print "friend1_sfriend2_p:", friend1_sfriend2_p
-        print "friend2_sfriend1_p:", friend2_sfriend1_p 
-        f.generate_reencrypt_key(friend1s, friend2p, friend1_sfriend2_p)
-        f.generate_reencrypt_key(friend2s, friend1p, friend2_sfriend1_p)
+#with open('100_node_links.txt', 'r') as fileInput:
+#    for line in fileInput:
+#        friends = line.split()
+#        friend1p = "files/public_keys/" + str(friends[0]) + "_p"
+#        friend1s = "files/public_keys/" + str(friends[0]) + "_s"
+#        friend2p = "files/public_keys/" + str(friends[1]) + "_p"
+#        friend2s = "files/public_keys/" + str(friends[1]) + "_s"
+#        print "friend1p:", friend1p
+#        print "friend1s:", friend1s
+#        print "friend2p:", friend2p
+#        print "friend2s:", friend2s
+#        friend1_sfriend2_p = "files/"+str(friends[0])+"/del_keys/"+str(friends[0])+"_s"+str(friends[1])+"_p"
+#        friend2_sfriend1_p = "files/"+str(friends[1])+"/del_keys/"+str(friends[1])+"_s"+str(friends[0])+"_p"
+#        print "friend1_sfriend2_p:", friend1_sfriend2_p
+#        print "friend2_sfriend1_p:", friend2_sfriend1_p 
+#        f.generate_reencrypt_key(friend1s, friend2p, friend1_sfriend2_p)
+#        f.generate_reencrypt_key(friend2s, friend1p, friend2_sfriend1_p)
 
