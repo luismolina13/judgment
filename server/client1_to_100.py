52980d3ddba39cfefd5c8073aa8f59b4d004d11c
Luis MOlina <luismol@umich.edu>
2015-04-09 14:23:09 +0000

    created server and client files for the 1-to-100 graph

---
+++ server/client1_to_100.py
@@ -0,0 +1,290 @@
+import bitly_api
+import csv
+import datetime
+import operator
+import os
+import requests
+import shutil
+import threading
+import time
+import tinyurl
+import urllib2
+import uuid
+import zipfile
+
+
+from proxylib import Proxylib
+from random import randint
+
+BITLY_ACCESS_TOKEN = "84b5098ee1dc11ffe4d23e0999846c62c84a49bd"
+url_threshold = 1
+proxylib = Proxylib()
+
+client_id = 0
+# Lisa's Server
+#server_url = 'http://ec2-54-178-192-75.ap-northeast-1.compute.amazonaws.com/'
+# Luis' Server
+server_url = 'http://ec2-52-68-29-226.ap-northeast-1.compute.amazonaws.com/'
+
+
+def setInterval(interval, times = -1):
+    # This will be the actual decorator,
+    # with fixed interval and times parameter
+    def outer_wrap(function):
+        # This will be the function to be
+        # called
+        def wrap(*args, **kwargs):
+            stop = threading.Event()
+
+            # This is another function to be executed
+            # in a different thread to simulate setInterval
+            def inner_wrap():
+                i = 0
+                while i != times and not stop.isSet():
+                    stop.wait(interval)
+                    function(*args, **kwargs)
+                    i += 1
+
+            t = threading.Timer(0, inner_wrap)
+            t.daemon = True
+            t.start()
+            return stop
+        return wrap
+    return outer_wrap
+
+def upload(urls, encr_stats, upld_stats):
+    #client_id = randint(0,99)
+    print 'Uploading urls for client ' + str(client_id)
+    # Encrypt the file
+    filename = str(uuid.uuid4())
+
+    encr_begin = time.time()
+    proxylib.encrypt('files/public_keys/' + str(client_id) + '_p', str(urls), filename)
+    encr_time = time.time() - encr_begin
+    print encr_stats['avg']
+    encr_stats['avg'] += encr_time
+    encr_stats['count'] += 1
+    if encr_time < encr_stats['min']:
+        encr_stats['min'] = encr_time
+    if encr_time > encr_stats['max']:
+        encr_stats['max'] = encr_time
+
+    filehandle = open(filename)
+    upload_url = server_url + 'upload/' + str(client_id)
+    upload_begin = time.time()
+    r = requests.post(upload_url, data={},files = {'file':filehandle})
+    print r.status_code
+    upload_time = time.time() - upload_begin
+    print upld_stats['avg']
+    upld_stats['avg'] += upload_time
+    upld_stats['count'] += 1
+    if upload_time < upld_stats['min']:
+        upld_stats['min'] = upload_time
+    if upload_time > upld_stats['max']:
+        upld_stats['max'] = upload_time
+    print r.text
+    # Delete the file
+    os.remove(filename)
+
+def download(down_stats, client_id):
+    print 'Downloading files'
+    download_url = server_url + 'download/' + str(client_id)
+    download_begin = time.time()
+    r = requests.get(download_url)
+    if r.status_code == 204:
+        return None
+
+    filename = str(uuid.uuid4())
+    with open(filename + '.zip', 'w') as f:
+        f.write(r.content)
+
+    download_time = time.time() - download_begin
+    print down_stats['avg']
+    down_stats['avg'] += download_time
+    down_stats['count'] += 1
+    if download_time < down_stats['min']:
+        down_stats['min'] = download_time
+    if download_time > down_stats['max']:
+        down_stats['max'] = download_time
+
+    return filename
+    # try:
+    #     response = http_client.fetch(url)
+    #     with open("file" + str(count) + ".zip", 'w') as f:
+    #     f.write(response.body)
+    #     count += 1
+    # except httpclient.HTTPError as e:
+    #     print("Error:", e)
+    # http_client.close()
+
+
+def parse_history(history_file):
+    urls = []
+    with open(history_file, 'rb') as csvfile:
+        historyreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in historyreader:
+            urls.append(row[1])
+    return urls
+
+def print_url_stats(cid, friends_urls, most_recent, resolve_urls=False):
+    print "########## Most Recent Urls for %d ##########" % cid
+    for url in most_recent:
+        if resolve_urls:
+            try:
+                response = urllib2.urlopen(url)
+                print response.url
+            except urllib2.HTTPError as err:
+                print err
+                print url
+        else:
+            print url
+    print "#############################################"
+
+    print "############ Top 10 Urls for %d #############" % cid
+    sorted_by_count = sorted(friends_urls.items(), key=operator.itemgetter(1))
+    sorted_by_count.reverse()
+    length = 10
+    if len(sorted_by_count) < 10:
+        length = len(sorted_by_count)
+    for num in range(0, length):
+        if resolve_urls:
+            try:
+                response = urllib2.urlopen(sorted_by_count[num][0])
+                print response.url, sorted_by_count[num][1]
+            except urllib2.HTTPError as err:
+                print err
+                print sorted_by_count[num][0], sorted_by_count[num][1]
+        else:
+            print sorted_by_count[num]
+    print "#############################################"
+
+
+def print_and_write(name, stats):
+    avg = 0.0
+    if stats['count']:
+        avg = stats['avg'] / stats['count']
+    else:
+        stats['min'] = 0.0
+    print name, "count:", stats['count']
+    print name, "average:", avg
+    print name, "min:", stats['min']
+    print name, "max:", stats['max']
+    filename = name + "_times_1_to_100.txt"
+    with open(filename, 'a') as wfile:
+        wfile.write("%d,%f,%f,%f\n" % (stats['count'], avg, stats['min'], stats['max']))
+
+def main():
+    client_urls = dict.fromkeys(range(0,100), dict())
+    for i in range(0, 100):
+        client_urls[i]['most_recent'] = []
+        client_urls[i]['friends_urls'] = {}
+
+    urls = parse_history('luis_history.csv')
+    #urls = []#'http://he11oworld.comhe11oworld.comhe11oworld.comhe11oworld.comhe11oworld.comhe11oworld.com']#, 'http://superuser.com/questions/can-chrome-browser-history-be-exported-to-an-html-file']
+
+    # count, min, max, avg
+    upld_stats = {'count': 0, 'min': 1.0, 'max': 0.0, 'avg': 0.0}
+    encr_stats = {'count': 0, 'min': 1.0, 'max': 0.0, 'avg': 0.0}
+    down_stats = {'count': 0, 'min': 1.0, 'max': 0.0, 'avg': 0.0}
+    decr_stats = {'count': 0, 'min': 1.0, 'max': 0.0, 'avg': 0.0}
+    # download file counts
+    durl_stats = {'count': 0, 'min': 1000, 'max': 0, 'avg': 0.0}
+
+    @setInterval(1)
+    def send_urls():
+        if len(urls) == 0:
+            return        
+        url_for_tiny = urls.pop(0)
+        url = url_for_tiny.split("://")
+        if len(url) > 1:
+            if 'www.google' in url[1]:
+                pass
+            else:
+                bitly = bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
+                data = bitly.shorten(url_for_tiny)
+                print data
+                tiny_url = data['url']
+                if len(tiny_url) > 132:
+                    # TODO: convert to tiny url
+                    tiny_url = tiny_url[0:132]
+                print "============> ", url[1], tiny_url, len(tiny_url)
+                upload(tiny_url, encr_stats, upld_stats)
+
+    @setInterval(5)
+    def get_urls():
+        #client_id = randint(0,99)
+        filename = download(down_stats, client_id)
+        if not filename:
+            return
+        zipf = zipfile.ZipFile(filename + '.zip')
+        zipf.extractall(filename)
+        url_num = len(os.listdir(filename + '/downloads/'))
+        print durl_stats['avg']
+        durl_stats['avg'] += url_num
+        durl_stats['count'] += 1
+        if url_num < durl_stats['min']:
+            durl_stats['min'] = url_num
+        if url_num > durl_stats['max']:
+            durl_stats['max'] = url_num
+        
+        decr_begin = time.time()
+        for file_name in os.listdir(filename + '/downloads/'):
+            decrypted_file = str(uuid.uuid4())
+            proxylib.decrypt('files/public_keys/' + str(client_id) + '_s', filename + '/downloads/' + file_name, filename + '/' + decrypted_file)
+            with open(filename + '/' + decrypted_file, 'r') as df:
+                dec_urls = df.readlines()
+                for dec_url in dec_urls:
+                    #fp = urllib2.urlopen(dec_url)
+                    print "<============ ", dec_url#, fp.geturl()
+                    if dec_url in client_urls[client_id]['friends_urls']:
+                        client_urls[client_id]['friends_urls'][dec_url] += 1
+                    else:
+                        client_urls[client_id]['friends_urls'][dec_url] = 1
+                    client_urls[client_id]['most_recent'].insert(0, dec_url)
+                    if len(client_urls[client_id]['most_recent']) > 10:
+                        client_urls[client_id]['most_recent'].pop()
+        decr_time = time.time() - decr_begin 
+        print decr_stats['avg']
+        decr_stats['avg'] += decr_time
+        decr_stats['count'] += 1
+        if decr_time < decr_stats['min']:
+            decr_stats['min'] = decr_time
+        if decr_time > decr_stats['max']:
+            decr_stats['max'] = decr_time
+
+        shutil.rmtree(filename)
+        os.remove(filename + '.zip')
+
+        # Print the top 10 urls and the 10 most recent
+        print_url_stats(client_id, client_urls[client_id]['friends_urls'], client_urls[client_id]['most_recent'])
+
+    send_urls()
+    #get_urls()
+    time.sleep(16)
+    
+    print_and_write('Upload', upld_stats)
+    print_and_write('Encryption', encr_stats)
+    print_and_write('Download', down_stats)
+    print_and_write('Decryption', decr_stats)
+    print_and_write('Num_download_files', durl_stats)
+
+    # @setInterval(1)
+    # def foo(a):
+    #     print(a)
+
+    # foo('bar')
+    # time.sleep(5)
+    # Will print 'bar' 3 times with 1 second delays
+
+    # time.sleep(5)
+    # stopper.set()
+    # It will stop here, after printing 'bar' 5 times.
+    # while True:
+    #     time.sleep(1)
+    #     print count
+    #     count += 1
+
+    #print_url_stats(friends_urls, most_recent, True)
+
+if __name__ == "__main__":
+    main()
---
+++ server/server1_to_100.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import tornado.ioloop
+import tornado.web
+import os, uuid
+from proxylib import Proxylib
+import zipfile
+import time
+import datetime
+ 
+__UPLOADS__ = "uploads/"
+__DOWNLOADS__ = "downloads/"
+__FILES__ = "1_to_100/"
+__DEL_KEYS__ = "del_keys/"
+proxylib = Proxylib()
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+         self.write("Hello, world")
+        #self.render("openID_login.html")
+
+class UploadHandler(tornado.web.RequestHandler):
+    def get(self, user_id):
+        self.write("Upload a file with POST for user: %s" %user_id)
+
+    def post(self, user_id):
+        fileinfo = self.request.files['file'][0]
+        #print "fileinfo is", fileinfo
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        fname += extn
+        directory = __FILES__ + user_id + "/" + __UPLOADS__
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(directory + fname, 'w') as fh:
+            fh.write(fileinfo['body'])
+        fh.close()
+
+        # Reencrypt the file I just received
+        re_encrypt_start = time.time()
+        del_key_dir = __FILES__ + user_id + "/" + __DEL_KEYS__
+        for del_key in os.listdir(del_key_dir):
+            friend_id = del_key.split('_')[1][1:]
+            #print "friend_id:", friend_id
+            download_directory = __FILES__ + friend_id + "/" + __DOWNLOADS__
+            if not os.path.exists(download_directory):
+                os.makedirs(download_directory)
+            proxylib.reencrypt(str(del_key_dir + del_key), str(directory+fname), str(download_directory + fname))
+        os.remove(str(directory + fname))
+        #t = datetime.datetime.fromtimestamp(re_encrypt_start).strftime('%Y-%m-%d %H:%M:%S')
+        t = time.time() - re_encrypt_start
+        print "Seconds for re-encryption:", t
+        with open('re-encryption_times_1_to_100.txt', 'a') as wfile:
+            wfile.write("%f\n" % t)
+        #proxylib.decrypt("FriendKey_s", "reencryption_file", "decryption")
+
+        #  stored in friends download folders to be used
+        self.finish(fname + " is uploaded!!")
+
+class DownloadHandler(tornado.web.RequestHandler):
+    '''Creates a .zip file that contains the downloads folder with all the
+       files inside of it.'''
+    def get(self, user_id):
+        files_dir = __FILES__ + user_id + '/'
+        buf_size = 4096
+        self.set_header('Content-Type', 'application/octet-stream')
+        self.set_header('Content-Disposition', 'attachment; filename=downloads.zip')
+
+        os.chdir(files_dir)
+        
+        zip_dir = str(files_dir + 'downloads.zip')
+        zipf = zipfile.ZipFile('downloads.zip', 'a')
+        if not os.listdir('downloads/'):
+            # No content status 204
+            self.set_status(204)
+            self.finish()
+            os.chdir('../..')
+            return
+        download_files = os.listdir('downloads/')
+        for download_file in download_files:
+            print os.getcwd()
+            zipf.write(str('downloads/' + download_file))
+        zipf.close()
+        
+        file_name = 'downloads.zip'
+        with open(file_name, 'r') as f:
+            while True:
+                data = f.read(buf_size)
+                if not data:
+                    break
+                self.write(data)
+        os.remove('downloads.zip')
+        for download_file in download_files:
+            os.remove(str('downloads/' + download_file))
+        os.chdir('../..')
+        self.finish()
+
+    def post(self, user_id):
+        fileinfo = self.request.files['file'][0]
+        print "fileinfo is", fileinfo
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        cname = str(uuid.uuid4()) + extn
+        fh = open(__UPLOADS__ + cname, 'w')
+        fh.write(fileinfo['body'])
+        self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/upload/([^/]+)", UploadHandler),
+    (r"/download/([^/]+)", DownloadHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
