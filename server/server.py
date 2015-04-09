f888201a4fad7988869facbb152060f4937ce0d3
Luis MOlina <luismol@umich.edu>
2015-04-09 20:26:49 +0000

    re-encryption changes and file name changes in server.py

--- server/server.py
+++ server/server.py
@@ -51,7 +51,7 @@ class UploadHandler(tornado.web.RequestHandler):
         #t = datetime.datetime.fromtimestamp(re_encrypt_start).strftime('%Y-%m-%d %H:%M:%S')
         t = time.time() - re_encrypt_start
         print "Seconds for re-encryption:", t
-        with open('re-encryption_times.txt', 'a') as wfile:
+        with open('re-encryption_times_parsed_csv.txt', 'a') as wfile:
             wfile.write("%f\n" % t)
         #proxylib.decrypt("FriendKey_s", "reencryption_file", "decryption")
 
